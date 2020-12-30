77e778cfc62daab72d9111d22c3638a4c2134641
Saketh Thota <saketht@umich.edu>
2020-12-30 14:49:20 -0600

    initial commit

---
+++ rice.sh
@@ -0,0 +1,17 @@
+# ubersicht
+brew install ubersicht
+
+# pecan 
+git clone https://github.com/zzzeyez/pecan.git "$HOME/Library/Application Support/Übersicht/widgets/pecan"
+mv "$HOME/Library/Application Support/Übersicht/widgets/pecan/config.css" "${HOME}/.config/pecan.css"
+
+# pywal *might not work*
+sudo pip3 install pywal
+
+# tty-clock
+brew install tty-clock
+
+# lazygit
+brew install lazygit
+
+
---
+++ setup.sh
@@ -0,0 +1,37 @@
+# xcode cli tools
+xcode-select --install
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+
+# iterm2
+brew install iterm2
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# git
+brew install git
+
+# vscode
+brew install visual-studio-code
+
+# alfred
+brew install alfred
+
+# firefox
+brew install firefox
+
+# notion
+brew install notion
+
+# spotify
+brew install spotify
+
+# neovim
+brew install vim neovim
+
+# tmux
+brew install tmux
+
