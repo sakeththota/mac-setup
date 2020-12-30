36383416428dca88022c8d881ff91ddde377c544
Saketh Thota <saketht@umich.edu>
2020-12-30 15:01:06 -0600

    added spicetify

--- rice.sh
+++ rice.sh
@@ -14,4 +14,13 @@ brew install tty-clock
 # lazygit
 brew install lazygit
 
+# spicetify
+brew install khanhas/tap/spicetify-cli
+spicetify
+spicetify backup apply enable-devtool
+spicetify update
+git clone https://github.com/morpheusthewhite/spicetify-themes.git ~/Desktop/spicetify-themes
+cd ~/Desktop/spicetify-themes
+cp -r * ~/.config/spicetify/Themes
+spicetify config current_theme Dracula
 
